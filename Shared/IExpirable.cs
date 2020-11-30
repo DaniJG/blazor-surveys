b99e5f068ee6ef7c1fa9c0ee6ffce1d4b3c68f17
Jimenez Garcia <daniel.garcia@oliverwyman.com>
2020-11-30 01:12:35 +0000

    simplify IExpirable properties

--- Shared/IExpirable.cs
+++ Shared/IExpirable.cs
@@ -6,7 +6,7 @@ namespace BlazorSurveys.Shared
     {
         DateTime ExpiresAt { get; }
         bool IsExpired => DateTime.Now > ExpiresAt;
-        int ExpiresInMin => (int)Math.Ceiling(ExpiresAt.Subtract(DateTime.Now).TotalMilliseconds / 60000);
+        int ExpiresInMin => (int)Math.Ceiling((decimal)ExpiresInSec / 60);
         int ExpiresInSec => (int)Math.Ceiling(ExpiresAt.Subtract(DateTime.Now).TotalMilliseconds / 1000);
     }
 }
